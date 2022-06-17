5c5a4e201562442a3cae307c45912060bf78bd0f
ihcho2 <101909278+ihcho2@users.noreply.github.com>
2022-06-17 10:43:05 -0500

    Update td_bert.py

--- models/td_bert.py
+++ models/td_bert.py
@@ -158,8 +158,10 @@ class BiGraphConvolution(nn.Module):
         denom1 = torch.sum(adj1, dim=2, keepdim=True)+1
         denom2 = torch.sum(adj2, dim=2, keepdim=True)+1
         
-        output = F.relu(torch.matmul(adj1, hidden) / denom1 + self.bias)
-        output2 = F.relu(torch.matmul(adj2, hidden2) /denom2 + self.bias2)
+        output = F.relu(torch.matmul(adj1, hidden) / denom1 + self.bias) + hidden
+        output= self.dropout(output)
+        output2 = F.relu(torch.matmul(adj2, hidden2) /denom2 + self.bias2) + hidden2
+        output2 = self.dropout(output2)
         
         output3 = torch.matmul(torch.cat((output,output2), dim=-1), self.weight3) + self.bias3
         
@@ -191,7 +193,8 @@ class TD_BERT_with_GCN(nn.Module):
             self.gcn3 = GraphConvolution(opt.gcn_hidden_dim, opt.gcn_hidden_dim)
             
         self.dropout = nn.Dropout(opt.keep_dropout)
-        self.fc = nn.Linear(embedding_dim+opt.gcn_hidden_dim, output_dim)
+        self.bert_fc = nn.Linear(embedding_dim+opt.gcn_hidden_dim, output_dim)
+        
         self.weight = nn.Parameter(torch.FloatTensor(768, opt.gcn_hidden_dim))
         
         self.init_parameters()
@@ -227,6 +230,7 @@ class TD_BERT_with_GCN(nn.Module):
                 tmps[i,j]=torch.sum(sentence_embed[i,span[0]+1:span[1]+1],0)
                 
         gcn_text = self.dropout(tmps)
+        
         gcn_text = F.relu(torch.matmul(gcn_text, self.weight))
         gcn_text=self.dropout(F.relu(self.gcn1(gcn_text, dg, dg1))) + gcn_text
         gcn_text=self.dropout(F.relu(self.gcn2(gcn_text, dg, dg1))) + gcn_text
@@ -242,7 +246,217 @@ class TD_BERT_with_GCN(nn.Module):
         # Concatenate the output of BERT and GCN.
         concatenated_embed = torch.cat((cat, gcn_cat), dim=1)
         
-        logits = self.fc(concatenated_embed)
+        logits = self.bert_fc(concatenated_embed)
+        logits = torch.tanh(logits)
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss()  # Standard cross-entropy loss function
+            # loss_fct = CrossEntropyLoss_LSR(device=self.opt.device, para_LSR=self.opt.para_LSR)  # Loss function after label smoothing，para_LSR The interval is 0.1~0.9
+            loss = loss_fct(logits, labels)
+            return loss, logits
+        else:
+            return logits
+
+class BERT_FC_TD(nn.Module):
+    def __init__(self, config, opt):
+        super(BERT_FC_TD, self).__init__()
+        self.opt = opt
+        n_filters = opt.n_filters  # The number of convolution kernels
+        filter_sizes = opt.filter_sizes  # Convolution kernel size, multiple sizes are passed as a list
+        embedding_dim = opt.embed_dim  
+        output_dim = opt.output_dim  # Output dimension, here is 3, representing negative, neutral, positive respectively
+        self.bert = BertModel(config)
+            
+        self.dropout = nn.Dropout(opt.keep_dropout)
+        self.bert_fc = nn.Linear(2*embedding_dim, output_dim)
+        
+    def forward(self, input_ids, token_type_ids, attention_mask, labels=None, input_t_ids=None, input_t_mask=None,
+                segment_t_ids=None, input_left_ids=None, input_left_mask=None, segment_left_ids=None, dg= None, dg1=None, tran_indices=None, span_indices=None):
+        all_encoder_layers, pooled_output = self.bert(input_ids, token_type_ids, attention_mask)
+        sentence_embed = all_encoder_layers[-1]  # Use the last layer of encoding results for classification
+        # sentence_embed = sum(all_encoder_layers)  # All layers are superimposed, I have tried it, the effect is not good, the convergence is slow, and the upper limit is low
+        
+                
+        target_in_sent_embed = torch.zeros(input_ids.size()[0], sentence_embed.size()[-1]).to(
+            self.opt.device)  # The embedding vector of the target word in the sentence
+        left_len = torch.sum(input_left_ids != 0, dim=-1) - 1  # Note that there are [CLS] and [SEP] at the beginning and end
+        target_len = torch.sum(input_t_ids != 0, dim=1) - 2  # Note that there are [CLS] and [SEP] at the beginning and end
+        target_in_sent_idx = torch.cat([left_len.unsqueeze(-1), (left_len + target_len).unsqueeze(-1)], dim=-1)
+
+        for i in range(input_ids.size()[0]):  # iterate over each of the batches
+            target_embed = sentence_embed[i][target_in_sent_idx[i][0]:target_in_sent_idx[i][1]]  # batch_size * max_seq_len * embedding_dim
+            target_in_sent_embed[i] = torch.max(target_embed, dim=0)[0]  # Converted to 1 * embedding_dim, taking the maximum value is the best (max_pooling)
+            # target_in_sent_embed[i] = target_embed.sum(dim=0)  # sum
+            # target_in_sent_embed[i] = torch.mean(target_embed, dim=0)[0]  # average
+
+        cat = self.dropout(target_in_sent_embed)       # TD output
+        pooled_output = self.dropout(pooled_output)    # CLS output
+                             
+        # Concatenate the output of BERT and GCN.
+        concatenated_embed = torch.cat((pooled_output, cat), dim=1)
+        
+        logits = self.bert_fc(concatenated_embed)
+        logits = torch.tanh(logits)
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss()  # Standard cross-entropy loss function
+            # loss_fct = CrossEntropyLoss_LSR(device=self.opt.device, para_LSR=self.opt.para_LSR)  # Loss function after label smoothing，para_LSR The interval is 0.1~0.9
+            loss = loss_fct(logits, labels)
+            return loss, logits
+        else:
+            return logits
+        
+class BERT_FC_GCN(nn.Module):
+    def __init__(self, config, opt):
+        super(BERT_FC_GCN, self).__init__()
+        self.opt = opt
+        n_filters = opt.n_filters  # The number of convolution kernels
+        filter_sizes = opt.filter_sizes  # Convolution kernel size, multiple sizes are passed as a list
+        embedding_dim = opt.embed_dim  
+        output_dim = opt.output_dim  # Output dimension, here is 3, representing negative, neutral, positive respectively
+        self.bert = BertModel(config)
+        if opt.bigcn == str(True):
+            print('-'*77)
+            print('Using BiGCN')
+            print('-'*77)
+            self.gcn1 = BiGraphConvolution(opt.gcn_hidden_dim, opt.gcn_hidden_dim)
+            self.gcn2 = BiGraphConvolution(opt.gcn_hidden_dim, opt.gcn_hidden_dim)
+            self.gcn3 = BiGraphConvolution(opt.gcn_hidden_dim, opt.gcn_hidden_dim)
+        else:
+            print('-'*77)
+            print('Using GCN')
+            print('-'*77)
+            self.gcn1 = GraphConvolution(opt.gcn_hidden_dim, opt.gcn_hidden_dim)
+            self.gcn2 = GraphConvolution(opt.gcn_hidden_dim, opt.gcn_hidden_dim)
+            self.gcn3 = GraphConvolution(opt.gcn_hidden_dim, opt.gcn_hidden_dim)
+            
+        self.dropout = nn.Dropout(opt.keep_dropout)
+        self.bert_fc = nn.Linear(embedding_dim+opt.gcn_hidden_dim, output_dim)
+        
+        self.weight = nn.Parameter(torch.FloatTensor(768, opt.gcn_hidden_dim))
+        
+        self.init_parameters()
+    
+    def init_parameters(self):
+        torch.nn.init.xavier_uniform_(self.weight)
+        
+    def forward(self, input_ids, token_type_ids, attention_mask, labels=None, input_t_ids=None, input_t_mask=None,
+                segment_t_ids=None, input_left_ids=None, input_left_mask=None, segment_left_ids=None, dg= None, dg1=None, tran_indices=None, span_indices=None):
+        all_encoder_layers, pooled_output = self.bert(input_ids, token_type_ids, attention_mask)
+        sentence_embed = all_encoder_layers[-1]  
+        pooled_output = self.dropout(pooled_output)
+        
+        # 먼저 BERT tokenized => DG 크기로 수정해야 함. DGEDT를 따라 그냥 sum을 사용.
+        tmps=torch.zeros(input_ids.size()[0], dg[0].size(-1), 768).float().to(self.opt.device)
+        for i,spans in enumerate(tran_indices):
+            for j,span in enumerate(spans):
+                tmps[i,j]=torch.sum(sentence_embed[i,span[0]+1:span[1]+1],0)
+                
+        gcn_text = self.dropout(tmps)
+        
+        gcn_text = F.relu(torch.matmul(gcn_text, self.weight))
+        gcn_text=self.dropout(F.relu(self.gcn1(gcn_text, dg, dg1))) + gcn_text
+        gcn_text=self.dropout(F.relu(self.gcn2(gcn_text, dg, dg1))) + gcn_text
+        gcn_text=self.dropout(F.relu(self.gcn3(gcn_text, dg, dg1))) + gcn_text
+        
+        gcn_target_embed = torch.zeros(input_ids.size()[0], gcn_text.size()[-1]).float().to(self.opt.device)
+        for i in range(input_ids.size()[0]):
+            x = gcn_text[i, span_indices[i][0][0]:span_indices[i][0][1]]
+            gcn_target_embed[i] = torch.sum(x, 0)
+            
+        gcn_cat = self.dropout(gcn_target_embed)
+                             
+        # Concatenate the output of BERT and GCN.
+        concatenated_embed = torch.cat((pooled_output, gcn_cat), dim=1)
+        
+        logits = self.bert_fc(concatenated_embed)
+        logits = torch.tanh(logits)
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss()  # Standard cross-entropy loss function
+            # loss_fct = CrossEntropyLoss_LSR(device=self.opt.device, para_LSR=self.opt.para_LSR)  # Loss function after label smoothing，para_LSR The interval is 0.1~0.9
+            loss = loss_fct(logits, labels)
+            return loss, logits
+        else:
+            return logits
+        
+class BERT_FC_TD_GCN(nn.Module):
+    def __init__(self, config, opt):
+        super(BERT_FC_TD_GCN, self).__init__()
+        self.opt = opt
+        n_filters = opt.n_filters  # The number of convolution kernels
+        filter_sizes = opt.filter_sizes  # Convolution kernel size, multiple sizes are passed as a list
+        embedding_dim = opt.embed_dim  
+        output_dim = opt.output_dim  # Output dimension, here is 3, representing negative, neutral, positive respectively
+        self.bert = BertModel(config)
+        if opt.bigcn == str(True):
+            print('-'*77)
+            print('Using BiGCN')
+            print('-'*77)
+            self.gcn1 = BiGraphConvolution(opt.gcn_hidden_dim, opt.gcn_hidden_dim)
+            self.gcn2 = BiGraphConvolution(opt.gcn_hidden_dim, opt.gcn_hidden_dim)
+            self.gcn3 = BiGraphConvolution(opt.gcn_hidden_dim, opt.gcn_hidden_dim)
+        else:
+            print('-'*77)
+            print('Using GCN')
+            print('-'*77)
+            self.gcn1 = GraphConvolution(opt.gcn_hidden_dim, opt.gcn_hidden_dim)
+            self.gcn2 = GraphConvolution(opt.gcn_hidden_dim, opt.gcn_hidden_dim)
+            self.gcn3 = GraphConvolution(opt.gcn_hidden_dim, opt.gcn_hidden_dim)
+            
+        self.dropout = nn.Dropout(opt.keep_dropout)
+        self.bert_fc = nn.Linear(2*embedding_dim+opt.gcn_hidden_dim, output_dim)
+        
+        self.weight = nn.Parameter(torch.FloatTensor(768, opt.gcn_hidden_dim))
+        
+        self.init_parameters()
+    
+    def init_parameters(self):
+        torch.nn.init.xavier_uniform_(self.weight)
+        
+    def forward(self, input_ids, token_type_ids, attention_mask, labels=None, input_t_ids=None, input_t_mask=None,
+                segment_t_ids=None, input_left_ids=None, input_left_mask=None, segment_left_ids=None, dg= None, dg1=None, tran_indices=None, span_indices=None):
+        all_encoder_layers, pooled_output = self.bert(input_ids, token_type_ids, attention_mask)
+        sentence_embed = all_encoder_layers[-1]  # Use the last layer of encoding results for classification
+        # sentence_embed = sum(all_encoder_layers)  # All layers are superimposed, I have tried it, the effect is not good, the convergence is slow, and the upper limit is low
+        
+                
+        target_in_sent_embed = torch.zeros(input_ids.size()[0], sentence_embed.size()[-1]).to(
+            self.opt.device)  # The embedding vector of the target word in the sentence
+        left_len = torch.sum(input_left_ids != 0, dim=-1) - 1  # Note that there are [CLS] and [SEP] at the beginning and end
+        target_len = torch.sum(input_t_ids != 0, dim=1) - 2  # Note that there are [CLS] and [SEP] at the beginning and end
+        target_in_sent_idx = torch.cat([left_len.unsqueeze(-1), (left_len + target_len).unsqueeze(-1)], dim=-1)
+
+        for i in range(input_ids.size()[0]):  # iterate over each of the batches
+            target_embed = sentence_embed[i][target_in_sent_idx[i][0]:target_in_sent_idx[i][1]]  # batch_size * max_seq_len * embedding_dim
+            target_in_sent_embed[i] = torch.max(target_embed, dim=0)[0]  # Converted to 1 * embedding_dim, taking the maximum value is the best (max_pooling)
+            # target_in_sent_embed[i] = target_embed.sum(dim=0)  # sum
+            # target_in_sent_embed[i] = torch.mean(target_embed, dim=0)[0]  # average
+
+        cat = self.dropout(target_in_sent_embed)
+        pooled_output = self.dropout(pooled_output)
+        
+        # 먼저 BERT tokenized => DG 크기로 수정해야 함. DGEDT를 따라 그냥 sum을 사용.
+        tmps=torch.zeros(input_ids.size()[0], dg[0].size(-1), 768).float().to(self.opt.device)
+        for i,spans in enumerate(tran_indices):
+            for j,span in enumerate(spans):
+                tmps[i,j]=torch.sum(sentence_embed[i,span[0]+1:span[1]+1],0)
+                
+        gcn_text = self.dropout(tmps)
+        
+        gcn_text = F.relu(torch.matmul(gcn_text, self.weight))
+        gcn_text=self.dropout(F.relu(self.gcn1(gcn_text, dg, dg1))) + gcn_text
+        gcn_text=self.dropout(F.relu(self.gcn2(gcn_text, dg, dg1))) + gcn_text
+        gcn_text=self.dropout(F.relu(self.gcn3(gcn_text, dg, dg1))) + gcn_text
+        
+        gcn_target_embed = torch.zeros(input_ids.size()[0], gcn_text.size()[-1]).float().to(self.opt.device)
+        for i in range(input_ids.size()[0]):
+            x = gcn_text[i, span_indices[i][0][0]:span_indices[i][0][1]]
+            gcn_target_embed[i] = torch.sum(x, 0)
+            
+        gcn_cat = self.dropout(gcn_target_embed)
+                             
+        # Concatenate the output of BERT and GCN.
+        concatenated_embed = torch.cat((pooled_output, cat, gcn_cat), dim=1)
+        
+        logits = self.bert_fc(concatenated_embed)
         logits = torch.tanh(logits)
         if labels is not None:
             loss_fct = nn.CrossEntropyLoss()  # Standard cross-entropy loss function
