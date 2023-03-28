e910b56864155cf9cec1ca65522dc36333a63b75
ihcho2 <101909278+ihcho2@users.noreply.github.com>
2023-03-28 01:21:29 -0500

    Update save_and_load.py

--- utils/save_and_load.py
+++ utils/save_and_load.py
@@ -8,7 +8,7 @@ from torch import nn
 
 logger = logging.getLogger(__name__)
 
-def load_model_ER_1(model, checkpoint, mode='student', verbose=True, DEBUG=False):
+def load_model_roberta_star(model, checkpoint = None, mode='student', verbose=True, DEBUG=False):
     """
 
     :param model:
@@ -22,165 +22,210 @@ def load_model_ER_1(model, checkpoint, mode='student', verbose=True, DEBUG=False
     """
 
     local_rank = -1
-    if checkpoint in [None, 'None']:
-        if verbose:
-            logger.info('no checkpoint provided for %s!' % model._get_name())
-    else:
-        if not os.path.exists(checkpoint):
-            raise ValueError('checkpoint %s not exist' % checkpoint)
-        if verbose:
-            logger.info('loading %s finetuned model from %s' % (model._get_name(), checkpoint))
-        model_state_dict = torch.load(checkpoint)
-#         old_keys = []
-#         new_keys = []
-#         for key in model_state_dict.keys():
-#             new_key = None
-#             if 'gamma' in key:
-#                 new_key = key.replace('gamma', 'weight')
-#             if 'beta' in key:
-#                 new_key = key.replace('beta', 'bias')
-#             if key.startswith('module.'):
-#                 new_key = key.replace('module.', '')
-#             if new_key:
-#                 old_keys.append(key)
-#                 new_keys.append(new_key)
-#         for old_key, new_key in zip(old_keys, new_keys):
-#             model_state_dict[new_key] = model_state_dict.pop(old_key)
-            
-#         model_state_dict_org = model_state_dict.copy()
-
-        del_keys = []
-        keep_keys = []
-        
-        
-        model_keys = model.state_dict().keys()
-        
-#         if student_layer_initialization != None:
-#             assert args.student_hidden_layers == len(student_layer_initialization)
-
-#             for keys in model_keys:
-#                 if 'scc' in keys:
-#                     is_Theseus = True
-
-#             if is_Theseus == False:
-#                 for i in range(len(student_layer_initialization)):
-#                     for keys in model_keys:
-#                         if f'bert.encoder.layer.{i}.' in keys:
-#                             model_state_dict[keys] = model_state_dict_org[keys.replace(str(i), 
-#                                                                                       str(student_layer_initialization[i]-1))]
-#             else:
-#                 for i in range(len(student_layer_initialization)):
-#                     for keys in model_keys:
-#                         if f'bert.encoder.scc_layer.{i}.' in keys:
-#                             model_state_dict[keys] = model_state_dict_org[keys.replace(f'scc_layer.{i}', 
-#                                                                               f'layer.{student_layer_initialization[i]-1}')]
-
-#         for t in list(model_state_dict.keys()):
-#             if t not in model_keys:
-#                 del model_state_dict[t]
-#                 del_keys.append(t)
-#             else:
-#                 keep_keys.append(t)
-        
-        model.load_state_dict(model_state_dict)
-        
-#         if student_layer_initialization != None:
-#             if is_Theseus == False:
-#                 for keys in model_state_dict.keys():
-#                     for i in range(len(student_layer_initialization)):
-#                         if '.'+str(i)+'.' in keys:
-#                             assert model_state_dict[keys].mean() == model_state_dict_org[keys.replace(str(i),                                                                                           str(student_layer_initialization[i]-1))].mean()
-#             else:
-#                 for keys in model_state_dict.keys():
-#                     for i in range(len(student_layer_initialization)):
-#                         if '.layer.'+str(i)+'.' in keys:
-#                             assert model_state_dict[keys].mean() == model_state_dict_org[keys].mean()
-#                         if '.scc_layer.'+str(i)+'.' in keys:
-#                             assert model_state_dict[keys].mean() == model_state_dict_org[keys.replace(f'scc_layer.{i}',
-#                                                                             f'layer.{student_layer_initialization[i]-1}')].mean()
-                    
-#         logger.info('delete %d layers, keep %d layers' % (len(del_keys), len(keep_keys)))
-        
-    return model
-
-def load_model_roberta_rpt(model, checkpoint1, checkpoint2, checkpoint3, checkpoint4=None, mode='student', verbose=True, DEBUG=False):
-    """
-
-    :param model:
-    :param checkpoint:
-    :param argstrain:
-    :param mode:  this is created because for old training the encoder and classifier are mixed together
-                  also adding student mode
-    :param train_mode:
-    :param verbose:
-    :return:
-    """
-
-    local_rank = -1
-    if checkpoint1 in [None, 'None']:
-        if verbose:
-            logger.info('no checkpoint provided for %s!' % model._get_name())
-    else:
-        if not os.path.exists(checkpoint1):
-            raise ValueError('checkpoint %s not exist' % checkpoint1)
-        if verbose:
-            logger.info('loading %s finetuned model from %s' % (model._get_name(), checkpoint1))
-        model_state_dict_1 = torch.load(checkpoint1)
-        model_state_dict_2 = torch.load(checkpoint2)
-        model_state_dict_3 = torch.load(checkpoint3)
-        if checkpoint4 != None:
-            model_state_dict_4 = torch.load(checkpoint4)
-        
-#         model_state_dict_1_ = {}
-#         model_state_dict_2_ = {}
-#         model_state_dict_3_ = {}
-        
-#         for key in model_state_dict_1.keys():
-#             new_key = 'bert.'+key
-#             model_state_dict_1_[new_key] = model_state_dict_1[key]
-            
-#         for key in model_state_dict_2.keys():
-#             new_key = 'bert.'+key
-#             model_state_dict_2_[new_key] = model_state_dict_2[key]
-            
-#         for key in model_state_dict_3.keys():
-#             new_key = 'bert.'+key
-#             model_state_dict_3_[new_key] = model_state_dict_3[key]
-
-        model_keys = model.state_dict().keys()
-        
-        merged_model_state_dict = model_state_dict_1.copy()
-        
-        
-        
-#         for key in model_keys:
-#             for i in range(12,24):
-#                 if '.'+str(i)+'.' in key:
-#                     merged_model_state_dict[key] = model_state_dict_2[key.replace(f'bert.encoder.layer.{i}.',
-#                                                                                   f'encoder.layer.{i-12}.')]
-#             for i in range(24,36):
-#                 if '.'+str(i)+'.' in key:
-#                     merged_model_state_dict[key] = model_state_dict_3[key.replace(f'bert.encoder.layer.{i}.',
-#                                                                                   f'encoder.layer.{i-24}.')]
-                    
+    model_keys = model.state_dict().keys()
+    model_state_dict = model.state_dict()
+    final_model_state_dict = model_state_dict.copy()
+    
+    if checkpoint == None:
+        print('='*100)
         for key in model_keys:
-            for i in range(12,24):
-                if '.'+str(i)+'.' in key:
-                    merged_model_state_dict[key] = model_state_dict_2[key.replace(f'.{i}.', f'.{i-12}.')]
-            for i in range(24,36):
-                if '.'+str(i)+'.' in key:
-                    merged_model_state_dict[key] = model_state_dict_3[key.replace(f'.{i}.', f'.{i-24}.')]
-            if checkpoint4 != None:
-                for i in range(36,48):
-                    if '.'+str(i)+'.' in key:
-                        merged_model_state_dict[key] = model_state_dict_4[key.replace(f'.{i}.', f'.{i-36}.')]
+            if 'g_star' in key:
+                print(key)
+                new_key = key.replace('_g_star','')
+                print(f'Changed from {torch.sum(final_model_state_dict[key])} to {torch.sum(model_state_dict[new_key])}')
+                final_model_state_dict[key] = model_state_dict[new_key]
+                print('-'*77)
+            elif 'self_star' in key:
+                print(key)
+                new_key = key.replace('self_star', 'self')
+                print(f'Changed from {torch.sum(final_model_state_dict[key])} to {torch.sum(model_state_dict[new_key])}')
+                final_model_state_dict[key] = model_state_dict[new_key]
+                print('-'*77)
                 
-        model.load_state_dict(merged_model_state_dict)
+            for i in range(12):
+                if f'layer.{12+i}' in key:
+                    print(key)
+                    new_key = key.replace(f'layer.{12+i}', f'layer.{i}')
+                    print(f'Changed from {torch.sum(final_model_state_dict[key])} to {torch.sum(model_state_dict[new_key])}')
+                    final_model_state_dict[key] = model_state_dict[new_key]
+                    print('-'*77)
+                
+                
+    else:
+        model_state_dict_checkpoint = torch.load(checkpoint)
+        print('='*100)
+        for key in model_keys:
+            if 'g_star' in key:
+                print(key)
+                new_key = key.replace('_g_star', '')
+                print(f'Changed from {torch.sum(final_model_state_dict[key])} to {torch.sum(model_state_dict_checkpoint[new_key])}')
+                final_model_state_dict[key] = model_state_dict_checkpoint[new_key]
+                print('-'*77)
+            elif 'self_star' in key:
+                print(key)
+                new_key = key.replace('self_star', 'self')
+                print(f'Changed from {torch.sum(final_model_state_dict[key])} to {torch.sum(model_state_dict_checkpoint[new_key])}')
+                final_model_state_dict[key] = model_state_dict_checkpoint[new_key]
+                print('-'*77)
+            else:
+                final_model_state_dict[key] = model_state_dict_checkpoint[key]
+        
+    model.load_state_dict(final_model_state_dict)
         
     return model
 
 
-def load_model_roberta_distinct(model, checkpoint1, checkpoint2=None, checkpoint3=None, checkpoint4=None, mode='student', verbose=True, DEBUG=False):
+def load_model_roberta_VDC_auto(model, checkpoint = None, mode='student', verbose=True, DEBUG=False):
+    """
+
+    :param model:
+    :param checkpoint:
+    :param argstrain:
+    :param mode:  this is created because for old training the encoder and classifier are mixed together
+                  also adding student mode
+    :param train_mode:
+    :param verbose:
+    :return:
+    """
+
+    local_rank = -1
+    model_keys = model.state_dict().keys()
+    model_state_dict = model.state_dict()
+    final_model_state_dict = model_state_dict.copy()
+    
+    if checkpoint == None:
+        print('='*100)
+        for key in model_keys:
+#             print(key)
+#             if '.attention.self.dense.weight' in key:
+#                 print(key)
+#                 pooler_key = key.replace('dense', 'value')
+# #                 pooler_key = 'roberta.pooler.dense.weight'
+#                 final_model_state_dict[key] = model_state_dict[pooler_key]
+#                 print('-'*77)
+#             elif '.attention.self.dense.bias' in key:
+#                 print(key)
+#                 pooler_key = key.replace('dense', 'value')
+# #                 pooler_key = 'roberta.pooler.dense.bias'
+#                 final_model_state_dict[key] = model_state_dict[pooler_key]
+#                 print('-'*77)
+                
+#             if 'replicate_query.0.' in key:
+#                 print('key: ', key)
+#                 new_key = key.replace('replicate_query.0.', 'value.')
+#                 print('new_key: ', new_key)
+#                 final_model_state_dict[key] = model_state_dict[new_key]
+#                 print('-'*77)
+#             elif 'replicate_query.1.' in key:
+#                 print('key: ', key)
+#                 new_key = key.replace('replicate_query.1.', 'value.')
+#                 print('new_key: ', new_key)
+#                 final_model_state_dict[key] = model_state_dict[new_key]
+#                 print('-'*77)
+#             elif 'replicate_query.2.' in key:
+#                 print('key: ', key)
+#                 new_key = key.replace('replicate_query.2.', 'value.')
+#                 print('new_key: ', new_key)
+#                 final_model_state_dict[key] = model_state_dict[new_key]
+#                 print('-'*77)
+#             elif 'replicate_query.3.' in key:
+#                 print('key: ', key)
+#                 new_key = key.replace('replicate_query.3.', 'value.')
+#                 print('new_key: ', new_key)
+#                 final_model_state_dict[key] = model_state_dict[new_key]
+#                 print('-'*77)
+#             elif 'replicate_query.4.' in key:
+#                 print('key: ', key)
+#                 new_key = key.replace('replicate_query.4.', 'value.')
+#                 print('new_key: ', new_key)
+#                 final_model_state_dict[key] = model_state_dict[new_key]
+#                 print('-'*77)
+#             elif 'replicate_query.5.' in key:
+#                 print('key: ', key)
+#                 new_key = key.replace('replicate_query.5.', 'value.')
+#                 print('new_key: ', new_key)
+#                 final_model_state_dict[key] = model_state_dict[new_key]
+#                 print('-'*77)
+                
+                
+                
+            if 'dense_s.' in key:
+                print('key: ', key)
+                new_key = key.replace('dense_s.', 'value.')
+                print('new_key: ', new_key)
+                final_model_state_dict[key] = model_state_dict[new_key]
+                print('-'*77)
+            elif 'dense_g.' in key:
+                print('key: ', key)
+                new_key = key.replace('dense_g.', 'value.')
+                print('new_key: ', new_key)
+                final_model_state_dict[key] = model_state_dict[new_key]
+                print('-'*77)    
+            
+                
+                
+                
+#             if 'self_replicate.0.' in key:
+#                 print(key)
+#                 new_key = key.replace('self_replicate.0.', 'self.')
+#                 final_model_state_dict[key] = model_state_dict[new_key]
+#                 print('-'*77)
+#             if 'self_replicate.1.' in key:
+#                 print(key)
+#                 new_key = key.replace('self_replicate.1.', 'self.')
+#                 final_model_state_dict[key] = model_state_dict[new_key]
+#                 print('-'*77)
+#             if 'self_replicate.2.' in key:
+#                 print(key)
+#                 new_key = key.replace('self_replicate.2.', 'self.')
+#                 final_model_state_dict[key] = model_state_dict[new_key]
+#                 print('-'*77)
+                
+                
+    else:
+        model_state_dict_checkpoint = torch.load(checkpoint)
+        print('='*100)
+        for key in model_keys:
+            if 'g_star' in key:
+                print(key)
+                new_key = key.replace('_g_star', '')
+                print(f'Changed from {torch.sum(final_model_state_dict[key])} to {torch.sum(model_state_dict_checkpoint[new_key])}')
+                final_model_state_dict[key] = model_state_dict_checkpoint[new_key]
+                print('-'*77)
+            elif 'self_star' in key:
+                print(key)
+                new_key = key.replace('self_star', 'self')
+                print(f'Changed from {torch.sum(final_model_state_dict[key])} to {torch.sum(model_state_dict_checkpoint[new_key])}')
+                final_model_state_dict[key] = model_state_dict_checkpoint[new_key]
+                print('-'*77)
+            else:
+                final_model_state_dict[key] = model_state_dict_checkpoint[key]
+        
+    model.load_state_dict(final_model_state_dict)
+        
+    return model
+
+def load_model_roberta_auto(model, checkpoint):
+    """
+
+    :param model:
+    :param checkpoint:
+    :param argstrain:
+    :param mode:  this is created because for old training the encoder and classifier are mixed together
+                  also adding student mode
+    :param train_mode:
+    :param verbose:
+    :return:
+    """
+
+    local_rank = -1
+    model_state_dict = torch.load(checkpoint)
+    model.load_state_dict(model_state_dict)
+        
+    return model
+
+
+def load_model_roberta_FM(model, checkpoint1=None, checkpoint2=None, checkpoint3=None, checkpoint4=None, mode='student', verbose=True, DEBUG=False):
     """
 
     :param model:
@@ -203,19 +248,18 @@ def load_model_roberta_distinct(model, checkpoint1, checkpoint2=None, checkpoint
 
     print('='*100)
     for key in model_keys:
-        if 'intermediate_g' in key:
+        if 'intermediate_T' in key:
             print(key)
-            new_key = key.replace('intermediate_g','intermediate')
+            new_key = key.replace('intermediate_T','intermediate')
             final_model_state_dict[key] = model_state_dict[new_key]
-        if 'output_g' in key:
+        if 'output_T' in key:
             print(key)
-            new_key = key.replace('output_g','output')
+            new_key = key.replace('output_T','output')
             final_model_state_dict[key] = model_state_dict[new_key]
 
     model.load_state_dict(final_model_state_dict)
         
     return model
-
 
 def load_model_MoE(model, checkpoint1, checkpoint2, checkpoint3, checkpoint4=None, checkpoint5 =None, mode='student', verbose=True, DEBUG=False):
     """
