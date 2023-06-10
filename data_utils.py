480a31d877298203de1c06bc6fc7935a13e905c7
ihcho2 <101909278+ihcho2@users.noreply.github.com>
2023-06-10 15:01:58 -0500

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -3,51 +3,18 @@
 import os
 import pickle
 import numpy as np
-from generateGraph_spacy import tokenize,concat
 from tqdm import tqdm
 import re
 from transformers import BertTokenizer, BertModel
 from transformers.optimization import AdamW
+import argparse
+import spacy
+
 # from transformers.optimization import AdamW, WarmupLinearSchedule
 # tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 # tokenizer.
 #bert_model= BertModel.from_pretrained('bert-base-uncased')
-
-def load_word_vec(path, word2idx=None):
-    fin = open(path, 'r', encoding='utf-8', newline='\n', errors='ignore')
-    word_vec = {}
-    for line in fin:
-        tokens = line.rstrip().split()
-        if word2idx is None or tokens[0] in word2idx.keys():
-            try:
-                word_vec[tokens[0]] = np.asarray(tokens[1:], dtype='float32')
-            except:
-                continue
-    return word_vec
-
-
-def build_embedding_matrix(word2idx, embed_dim, type):
-    embedding_matrix_file_name = '{0}_{1}_embedding_matrix.pkl'.format(str(embed_dim), type)
-#    if os.path.exists(embedding_matrix_file_name):
-#        print('loading embedding_matrix:', embedding_matrix_file_name)
-#        embedding_matrix = pickle.load(open(embedding_matrix_file_name, 'rb'))
-#    else:
-    if True:
-        print('loading word vectors ...')
-        embedding_matrix = np.zeros((len(word2idx), embed_dim))  # idx 0 and 1 are all-zeros
-        embedding_matrix[1, :] = np.random.uniform(-1/np.sqrt(embed_dim), 1/np.sqrt(embed_dim), (1, embed_dim))
-        fname = './datasets/glove/glove.840B.300d.txt'
-        word_vec = load_word_vec(fname, word2idx=word2idx)
-        print('building embedding_matrix:', embedding_matrix_file_name)
-        for word, i in word2idx.items():
-            vec = word_vec.get(word)
-            if vec is not None:
-                # words not found in embedding index will be all-zeros.
-                embedding_matrix[i] = vec
-        pickle.dump(embedding_matrix, open(embedding_matrix_file_name, 'wb'))
-    return embedding_matrix
-
-
+    
 class Tokenizer(object):
     def __init__(self, word2idx=None,tokenizer=None):
         self.tokenizer=BertTokenizer.from_pretrained('bert-base-uncased')
@@ -78,18 +45,19 @@ class Tokenizer(object):
 
     def text_to_sequence(self, text, tran=False):
         text = text.lower().strip()
-        words = tokenize(text)
+        words = tokenize(text)  # This is the parser's tokenizer.
         trans=[]
         realwords=[]
 
         for word in words:
-            wordpieces=self.tokenizer.tokenize(word)
+            wordpieces=self.tokenizer.tokenize(word)  # Tokenize again with BERT's tokenizer.
             tmplen=len(realwords)
             
             realwords.extend(wordpieces)
             trans.append([tmplen,len(realwords)])
-#        unknownidx = 1_convert_token_to_id
-        #print(realwords)
+            
+            assert tmplen != realwords
+            
         sequence = [self.tokenizer._convert_token_to_id('[CLS]')]+[self.tokenizer._convert_token_to_id(w) for w in realwords]+[self.tokenizer._convert_token_to_id('[SEP]')]
         if len(sequence) == 0:
             sequence = [0]
@@ -112,6 +80,7 @@ def span(texts,aspect):
     startid=0
     aslen=len(tokenize(aspect))
     spans=[]
+    
     for idx,text in enumerate(texts):
         tmp=len(tokenize(text))
         startid+=tmp
@@ -119,62 +88,51 @@ def span(texts,aspect):
         if idx < len(texts)-1:
             startid+=aslen
             spans.append([tmp,startid])
+            
+            assert tmp != startid
+            
     return spans
+
+        
 class ABSADatesetReader:
     @staticmethod
-    def __read_text__(fnames):
-        text = ''
-        for fname in fnames:
-            fin = open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
-            lines = fin.readlines()
-            fin.close()
-            for i in range(0, len(lines), 3):
-                text_left = [s.lower().strip() for s in lines[i].split("$T$")]
-                aspect = lines[i + 1].lower().strip()
-#                update_edge(concat(text_left,aspect),edge_vocab)
-#                text_left, _, text_right = [s.lower().strip() for s in lines[i].partition("$T$")]
-#                aspect = lines[i + 1].lower().strip()
-                text_raw = concat(text_left,aspect)
-                text += text_raw + " "
-        #print(text)
-        return text
-
-    @staticmethod
-    def __read_data__(fname, tokenizer,fname1):
+    def __read_data__(fname, tokenizer, parser_info=None):
         fin = open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
         lines = fin.readlines()
         fin.close()
-        fin = open(fname+'.graph', 'rb')
-        fin1 = open(fname1+'.edgevocab', 'rb')
+        fin = open(fname + '_' + parser_info + '.graph', 'rb')
+        fin1 = open('./datasets/' + parser_info + '.global_edge_vocab', 'rb')
+        fin2 = open('./datasets/' + parser_info + '.global_pos_vocab', 'rb')
+        
         idx2gragh = pickle.load(fin)
         edgevocab=pickle.load(fin1)
+        posvocab=pickle.load(fin2)
         fin1.close()
+        fin2.close()
         fin.close()
 
 
         all_data = []
         for i in tqdm(range(0, len(lines), 3)):
-#            text_left, _, text_right = [s.lower().strip() for s in lines[i].partition("$T$")]
-#            aspect = lines[i + 1].lower().strip()
             text_left = [s.lower().strip() for s in lines[i].split("$T$")]
             aspect = lines[i + 1].lower().strip()
             polarity = lines[i + 2].strip()
+            
             span_indices=span(text_left,aspect)
             assert len(span_indices)>=1
             concats=concat(text_left,aspect)
+            
             text_indices, tran_indices = tokenizer.text_to_sequence(concats,True)
             context_indices = tokenizer.text_to_sequence(concats)
             aspect_indices = tokenizer.text_to_sequence(aspect)
             left_indices = tokenizer.text_to_sequence(concats)
+            
             polarity = int(polarity)+1
+            
             dependency_graph = idx2gragh[i]
+            
             assert len(idx2gragh[i][0])==len(tokenize(concats))
-           # print(tokenize(concats))
-#             print(span_indices)
-#             print(tran_indices)
-#             print(aspect)
-#             print(dependency_graph[0])
-            #a=input('fdfdf')
+            
             data = {
                 'text': tokenize(concats.lower().strip()),
                 'aspect': tokenize(aspect),
@@ -190,64 +148,8 @@ class ABSADatesetReader:
 
             all_data.append(data)
         return all_data
-    def __read_datas__(fname, tokenizer,fname1):
-        fin = open(fname, 'r', encoding='utf-8', newline='\n', errors='ignore')
-        lines = fin.readlines()
-        fin.close()
-        fin = open(fname1, 'r', encoding='utf-8', newline='\n', errors='ignore')
-        lines1 = fin.readlines()
-        fin.close()
-        fin = open(fname+'.graph', 'rb')
-        fin1 = open(fname+'.edgevocab', 'rb')
-        idx2gragh = pickle.load(fin)
-        edgevocab=pickle.load(fin1)
-        fin1.close()
-        fin.close()
-
-
-        all_data = []
-        for i in tqdm(range(0, len(lines), 1)):
-#            text_left, _, text_right = [s.lower().strip() for s in lines[i].partition("$T$")]
-#            aspect = lines[i + 1].lower().strip()
-#            text_left = [s.lower().strip() for s in lines[i].split("$T$")]
-#            aspect = lines[i + 1].lower().strip()
-            pola = lines1[i].strip()
-#            span_indices=span(text_left,aspect)
-#            assert len(span_indices)>=1
-            concats=re.sub(r' {2,}',' ',lines[i].lower().strip())
-            text_indices, tran_indices = tokenizer.text_to_sequence(concats,True)
-#            context_indices = tokenizer.text_to_sequence(concats)
-#            aspect_indices = tokenizer.text_to_sequence(aspect)
-#            left_indices = tokenizer.text_to_sequence(concats)
-            if pola=='negative':
-                polarity=0
-            elif pola=='neutral':
-                polarity=1
-            else:
-                polarity=2
-                
-            dependency_graph = idx2gragh[i]
-            assert len(idx2gragh[i][0])==len(tokenize(concats))
-#            print(tokenize(concats))
-#            print(span_indices)
-#            print(aspect)
-#            a=input('fdfdf')
-            data = {
-                'text': tokenize(concats.lower().strip()),
-                'aspect': None,
-                'text_indices': text_indices,
-                'tran_indices': tran_indices,
-                'context_indices': None,
-                'span_indices': None,
-                'aspect_indices': None,
-                'left_indices': None,
-                'polarity': polarity,
-                'dependency_graph': dependency_graph,
-            }
-
-            all_data.append(data)
-        return all_data
-    def __init__(self, dataset='rest14', embed_dim=300,change=False):
+    
+    def __init__(self, dataset='rest14', parser_info = None):
         print("preparing {0} dataset ...".format(dataset))
         fname = {
             'twitter': {
@@ -276,55 +178,77 @@ class ABSADatesetReader:
             }
 
         }
-#        text = ABSADatesetReader.__read_text__([fname[dataset]['train'], fname[dataset]['test']])
+        
         self.tokenizer = Tokenizer()
-#        if os.path.exists(dataset+'_word2idx.pkl'):
-#            print("loading {0} tokenizer...".format(dataset))
-#            with open(dataset+'_word2idx.pkl', 'rb') as f:
-#                 word2idx = pickle.load(f)
-#                 tokenizer = Tokenizer(word2idx=word2idx)
-#        else:
-#            tokenizer = Tokenizer()
-#            tokenizer.fit_on_text(text)
-#            with open(dataset+'_word2idx.pkl', 'wb') as f:
-#                 pickle.dump(tokenizer.word2idx, f)
-        self.edgevocab=pickle.load(open(fname[dataset]['train']+'.edgevocab', 'rb'))
-        self.embedding_matrix = build_embedding_matrix(self.tokenizer.tokenizer.vocab, embed_dim, dataset)
-        if change:
-            self.train_data = ABSADataset(ABSADatesetReader.__read_datas__(fname[dataset]['train'], self.tokenizer,fname[dataset]['test']))
-        else:
-            self.train_data = ABSADataset(ABSADatesetReader.__read_data__(fname[dataset]['train'], self.tokenizer,fname[dataset]['train']))
-            self.test_data = ABSADataset(ABSADatesetReader.__read_data__(fname[dataset]['test'], self.tokenizer,fname[dataset]['train']))
+        
+        self.train_data = ABSADataset(ABSADatesetReader.__read_data__(fname[dataset]['train'], self.tokenizer,
+                                                                      parser_info = parser_info))
+        self.test_data = ABSADataset(ABSADatesetReader.__read_data__(fname[dataset]['test'], self.tokenizer,
+                                                                     parser_info = parser_info))
+            
+
+def get_config():
+    parser = argparse.ArgumentParser()
+
+    ## Required parameters
+    parser.add_argument("--parser_info",
+                        default='spacy_sm_3.3.0',
+                        type=str,
+                        help="Parser you want to use.")
+    
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    tmp=ABSADatesetReader(dataset='twitter')
-    dataset='twitter'
-    with open(dataset+'_datas.pkl', 'wb') as f:
-                     pickle.dump(tmp, f)
+    
+    args = get_config()
+    
+    print('='*77)
+    print('args.parser_info: ', args.parser_info)
+    print('='*77)
+    
+    if 'spacy_sm' in args.parser_info:
+        nlp = spacy.load('en_core_web_sm')
+    elif 'spacy_lg' in args.parser_info:
+        nlp = spacy.load('en_core_web_lg')
+        
+    def tokenize(text):
+        text=text.strip()
+        text=re.sub(r' {2,}',' ',text)
+        document = nlp(text)
 
-    tmp=ABSADatesetReader(dataset='rest14')
+        return [token.text for token in document]
+    
+    def concat(texts,aspect):
+        source=''
+        splitnum=0
+
+        for i,text in enumerate(texts):
+            source+=text
+            splitnum+=len(tokenize(text))
+            if i <len(texts)-1:
+                source+=' '+aspect+' '
+                splitnum+=len(tokenize(aspect))
+
+        if splitnum!=len(tokenize(source.strip())):
+            print('ERROR')
+
+        return re.sub(r' {2,}',' ',source.strip())
+    
+    # 1. Laptop
+#     tmp=ABSADatesetReader(dataset='lap14', parser_info = args.parser_info)
+#     dataset='lap14'
+#     with open(dataset+'_datas_' + args.parser_info + '.pkl', 'wb') as f:
+#                      pickle.dump(tmp, f)
+    
+    # 2. Restaurant
+    tmp=ABSADatesetReader(dataset='rest14', parser_info = args.parser_info)
     dataset='rest14'
-    with open(dataset+'_datas.pkl', 'wb') as f:
-                     pickle.dump(tmp, f)
+    with open(dataset+'_datas_' + args.parser_info + '.pkl', 'wb') as f:
+        pickle.dump(tmp, f)
             
-    tmp=ABSADatesetReader(dataset='lap14')
-    dataset='lap14'
-    with open(dataset+'_datas.pkl', 'wb') as f:
-                     pickle.dump(tmp, f)
-            
-#     tmp=ABSADatesetReader(dataset='rest15')
-#     dataset='rest15'
-#     with open(dataset+'_datas.pkl', 'wb') as f:
+    # 3. Twitter
+#     tmp=ABSADatesetReader(dataset='twitter', parser_info = args.parser_info)
+#     dataset='twitter'
+#     with open(dataset+'_datas_' + args.parser_info + '.pkl', 'wb') as f:
 #                      pickle.dump(tmp, f)
-
-#     tmp=ABSADatesetReader(dataset='rest16')
-#     dataset='rest16'
-#     with open(dataset+'_datas.pkl', 'wb') as f:
-#                      pickle.dump(tmp, f)
-
-#    tmp=ABSADatesetReader(dataset='other',change=True)
-#    dataset='other'
-#    with open(dataset+'_datas.pkl', 'wb') as f:
-#                     pickle.dump(tmp, f)
-
-#tmp=pickle.load(open(dataset+'_datas.pkl', 'rb'))
-
